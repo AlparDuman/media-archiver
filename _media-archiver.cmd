7d7a464270d00e107e081d1a5b28a3b04bb18a54
AlparDuman <>
2025-05-18 04:44:17 +0200

    initial with license

---
+++ _media-archiver.cmd
@@ -0,0 +1,17 @@
+REM Copyright (C) 2025 Alpar Duman
+REM This file is part of media-archiver.
+REM 
+REM media-archiver is free software: you can redistribute it and/or modify
+REM it under the terms of the GNU General Public License version 3 as
+REM published by the Free Software Foundation.
+REM 
+REM media-archiver is distributed in the hope that it will be useful,
+REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+REM GNU General Public License for more details.
+REM 
+REM You should have received a copy of the GNU General Public License
+REM along with media-archiver. If not, see
+REM <https://github.com/AlparDuman/media-archiver/blob/main/LICENSE>
+REM else <https://www.gnu.org/licenses/>.
+
